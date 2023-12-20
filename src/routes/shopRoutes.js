a196a52082a9b4df7e252480caef33fb6e428a5b
juanjo <juanjo.mdt@gmail.com>
2023-12-20 00:38:20 -0300

    actualizacion de items en /home

--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const shopControllers = require('../../controllers/shopController')
 
 router.get('/', shopControllers.shop)
-router.get('/item',shopControllers.item) //router.get('/item:id',shopControllers.item)
+router.get('/item/:id',shopControllers.item) //router.get('/item:id',shopControllers.item)
 
 router.post('/item/add/:id',shopControllers.itemAdd)
 router.get('/cart',shopControllers.cart)
