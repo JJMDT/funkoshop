d769c69dd2c4a59a16c50bc70d0b7c18bec6a6cb
juanjo <juanjo.mdt@gmail.com>
2023-12-31 18:16:57 -0300

    se hizo dinamica la creacion de nuevos productos y se guardan en la base de datos , falta arreglar la ruta de las imagenes para que se muestren correctamente

---
+++ src/models/product.models.js
@@ -0,0 +1,52 @@
+const { conn } = require("../config/conn");
+
+const getAll = async () => {
+  try {
+    const [rows] =
+      await conn.query(`select p.*,c.category_name,l.licence_name from (product as p
+  left join category as c  on p.category_id = c.category_id)
+  left join licence as l on p.licence_id = l.licence_id  ;`);
+    return rows;
+  } catch (error) {
+    console.log("se produjo un error " + error);
+  } finally {
+    conn.releaseConnection();
+  }
+};
+
+const getOne = async (id) => {
+  try {
+    const [rows] = await conn.query(
+      `select * from product as p
+    inner join licence as l  on p.licence_id = l.licence_id where product_id = ? ;`,
+      id
+    );
+    return rows;
+  } catch (error) {
+    console.log("se produjo un error " + error);
+  } finally {
+    conn.releaseConnection();
+  }
+};
+const create = async (params) => {
+  try {
+    console.log("Descripción del producto:", params[0].product_description);
+
+    const [product] = await conn.query(
+      "INSERT INTO product ( product_name, product_description, price, stock,discount, sku, dues, image_front, image_back, category_id, licence_id) values ?;",
+      [params]
+    );
+
+    return product;
+  } catch (error) {
+    console.log(" error ! " + error);
+  } finally {
+    conn.releaseConnection();
+  }
+};
+
+module.exports = {
+  getAll,
+  getOne,
+  create,
+};
