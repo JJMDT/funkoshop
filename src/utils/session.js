6ce07858688588423c02e99828387eaea448e7d6
juanjo <juanjo.mdt@gmail.com>
2024-02-25 21:40:37 -0300

    actualizacion package json

--- src/utils/session.js
+++ src/utils/session.js
@@ -1,42 +1,44 @@
-// const session= require('express-session');
-// const {conn} = require('../config/conn');
-// const mysqlStore = require('express-mysql-session')(session);
-
-// //declaramos nuestra BBDD como store para almacernar las sesiones
-// const sessionStore = new mysqlStore({
-
-// }, conn);
+const session= require('express-session');
+const {conn} = require('../config/conn');
+const mysqlStore = require('express-mysql-session')(session);
+require('dotenv').config();  // habilita a usar las variables en nuestra conexion, estan en el archivo .env
 
 
-// module.exports = {
-//     // inicializa y configura la session
-//     initSession: () => {
-//         return session({
-//             secret: "mySecretKey", // clave secreta compartida entre el servidor y el cliente para proteger la integridad de las cookies de sesión
-//             resave: false,
-//             saveUninitialized: false,
-//             store: sessionStore, // Almacenamos las sesiones en la base de datos
-//             rolling: true,
-//             cookie: {
-//                 maxAge: 600000 // tiempo que dura la sesion en milisegundos
-//             },
-//         });
-//     },
-// };
+//declaramos nuestra BBDD como store para almacernar las sesiones
+const sessionStore = new mysqlStore({
 
+}, conn);
 
-const cookieSession = require('cookie-session');
 
 module.exports = {
-  // Inicializa y configura la sesión
-  initSession: () => {
-    return cookieSession({
-      name: 'session', // Nombre de la cookie
-      keys: ['mySecretKey'], // Claves secretas para firmar la cookie
-      maxAge: 600000, // Tiempo que dura la sesión en milisegundos
-      httpOnly: true, // Evita que la cookie sea accesible a través del navegador
-      sameSite: 'strict', // Refuerza la política de mismo sitio
-      secure: process.env.NODE_ENV === 'production', // Configura para "true" en producción para habilitar cookies seguras (HTTPS)
-    });
-  },
+    // inicializa y configura la session
+    initSession: () => {
+        return session({
+            secret:process.env.SESSION_NAME ,//"mySecretKey", // clave secreta compartida entre el servidor y el cliente para proteger la integridad de las cookies de sesión
+            resave: false,
+            saveUninitialized: false,
+            store: sessionStore, // Almacenamos las sesiones en la base de datos
+            rolling: true,
+            cookie: {
+                maxAge: 600000 // tiempo que dura la sesion en milisegundos
+            },
+        });
+    },
 };
+
+
+// const cookieSession = require('cookie-session');
+
+// module.exports = {
+//   // Inicializa y configura la sesión
+//   initSession: () => {
+//     return cookieSession({
+//       secret: 'session', // Nombre de la cookie
+//       keys: ['mySecretKey'], // Claves secretas para firmar la cookie
+//       maxAge: 600000, // Tiempo que dura la sesión en milisegundos
+//       httpOnly: true, // Evita que la cookie sea accesible a través del navegador
+//       sameSite: 'strict', // Refuerza la política de mismo sitio
+//       secure: process.env.NODE_ENV === 'production', // Configura para "true" en producción para habilitar cookies seguras (HTTPS)
+//     });
+//   },
+// };
